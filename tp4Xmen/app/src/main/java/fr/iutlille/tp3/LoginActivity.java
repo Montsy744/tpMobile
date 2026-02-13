733ec9e80496b41cafe172a077b295d1cb76194c
Hugo Straseele <hugo.straseele.etu@univ-lille.fr>
2026-02-13 09:10:01 +0100

    init du projet tp4 mobile

---
+++ tp4Xmen/app/src/main/java/fr/iutlille/tp3/LoginActivity.java
@@ -0,0 +1,34 @@
+package fr.iutlille.tp3;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        Button btnConnexion = findViewById(R.id.btnLog);
+        btnConnexion.setOnClickListener({
+                goToNew();
+        });
+        setTitle(getLocalClassName());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void goToNew(){
+        setContentView(R.layout.activity_new);
+    }
+}
